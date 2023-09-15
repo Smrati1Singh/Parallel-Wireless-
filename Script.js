ab02269a6f3335cad443f580a061e78a6f0c8bf0
Smrati1Singh <145092335+Smrati1Singh@users.noreply.github.com>
2023-09-15 15:32:07 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,24 @@
+let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("image");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2000); 
+}
+
+let searchForm = document.querySelector('.icons');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}   
